29836beb891f612098af52f5c9dd44f38e95f010
Christian <cristian001_13@hotmail.com>
2019-07-12 11:55:21 +0200

    gives Buzz when the value is multiple five GREEN!!

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -60,7 +60,7 @@ describe("FizzBuzz", function() {
         if (value % 3 === 0) {
             return 'Fizz';
 
-        } else if (value === 5) {
+        } else if (value % 5 === 0) {
             return 'Buzz';
 
         } else {
