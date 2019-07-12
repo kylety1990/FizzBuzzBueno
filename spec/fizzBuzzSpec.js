8bb8bcfe49006021391f18f29dd1f714dbe5deef
Christian <cristian001_13@hotmail.com>
2019-07-12 12:02:32 +0200

    gives FizzBuzz when the value is multiple five and three GREEN!!

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -66,12 +66,14 @@ describe("FizzBuzz", function() {
 
     function fizzBuzz(value) {
 
-        if (value % 3 === 0) {
-            return 'Fizz';
+        if (value % 3 === 0 && value % 5 === 0) {
+            return 'FizzBuzz';
 
         } else if (value % 5 === 0) {
             return 'Buzz';
 
+        } else if (value % 3 === 0) {
+            return 'Fizz'
         } else {
             return value;
 
